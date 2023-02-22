bd887392d464a01d4fff06725e1f432b07b23efb
Lucas Alves Ribeiro <lucas.ribeiro@goldensolutions.com.br>
2023-02-22 15:21:22 -0300

    Melhorando Upload da imagem

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import render_template,request,redirect,session,flash,url_for
+from flask import render_template,request,redirect,session,flash,url_for,send_from_directory
 from jogoteca import app,db
 from models import Jogos,Usuarios
 
@@ -30,7 +30,8 @@ def criar():
     db.session.commit()
 
     arquivo = request.files['arquivo']
-    arquivo.save(f'uploads/{arquivo.filename}')
+    upload_path = app.config['UPLOAD_PATH']
+    arquivo.save(f'{upload_path}/capa{novo_jogo.id}.jpg')
 
     return redirect(url_for('index'))
 
@@ -88,3 +89,8 @@ def logout():
     session['usuario_logado'] = None
     flash('Logout efetuado com sucesso!')
     return redirect(url_for('index'))
+
+
+@app.route('/uploads/<nome_arquivo>')
+def imagem(nome_arquivo):
+    return send_from_directory('uploads',nome_arquivo)
